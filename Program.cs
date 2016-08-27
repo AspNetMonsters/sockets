a1923a25f1b138f116ac203647d7cd2490045458
Simon Timms <stimms@gmail.com>
2016-08-27 09:34:06 -0600

    Initial, bad implementation

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Starting connections");
+            for(int i = 0; i<10; i++)
+            {
+                using(var client = new HttpClient())
+                {
+                    var result = client.GetAsync("http://aspnetmonsters.com").Result;
+                    Console.WriteLine(result.StatusCode);
+                }
+            }
+            Console.WriteLine("Connections done");
+        }
+    }
+}
